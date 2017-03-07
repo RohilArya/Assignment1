040f95a3370c89672245a924e1e14c7cf75b5f91
Harshan <harshan.mohanaraj@uoit.net>
2017-03-07 18:08:51 -0500

    Fixed trees

--- SpamEmail/src/sample/Train.java
+++ SpamEmail/src/sample/Train.java
@@ -9,39 +9,44 @@ import java.util.*;
  * Created by 100585195 on 3/4/2017.
  */
 public class Train {
-    public TreeMap<String,Integer> hamTree;
-    public TreeMap<String,Integer> spamTree;
-    public TreeMap<String, Double> HamProb;
-    public TreeMap<String, Double> SpamProb;
+    public Map<String,Integer> hamTree;
+    public Map<String,Integer> spamTree;
+    public Map<String, Double> HamProb;
+    public Map<String, Double> SpamProb;
     public TreeMap<String,Double> TotalProb;
 
-    public int Occurence;
-    public int countWord;
+
     public File file;
     public File file2;
     public int numFiles;
-
-
-    public double TrainHam() throws IOException {
-        TreeMap<String,Integer> hamTree= new TreeMap<>();
-        this.hamTree=hamTree;
-        processFileHam(file);
-        return 0.0;}
-
-    public double TrainSpam() throws IOException {
-        TreeMap<String,Integer>spamTree= new TreeMap<>();
-        this.spamTree=spamTree;
-        processFileSpam(file2);
-
-        return 0.0;}
-
 
     public Train(File file, File file2) throws IOException {
         this.file=file;
         this.file2=file2;
         TrainHam();
         TrainSpam();
+
     }
+
+    public void TrainHam() throws IOException {
+        hamTree=new TreeMap<>();
+        processFileHam(file);
+        System.out.println("HAM TREE:");
+        System.out.print(hamTree);
+        System.out.print(HamProb);
+
+    }
+
+    public void TrainSpam() throws IOException {
+        spamTree=new TreeMap<>();
+        processFileSpam(file2);
+        System.out.println("SPAM TREE:");
+        System.out.print(spamTree);
+
+    }
+
+
+
 
     public void processFileSpam(File file) throws IOException {
         if (file.isDirectory()) {
@@ -58,15 +63,15 @@ public class Train {
             while (scanner.hasNext()) {
                 String word = scanner.next();
                 if (isWord(word)) {
-                    if(isUniqueSpam(word,spamTree)){
+                    if(isUniqueSpam(word)){
                     }
                 }
             }
         }
-        ProbabilitySpam();
     }
 
     public void processFileHam(File file) throws IOException {
+        //System.out.println("processFile running");
         if (file.isDirectory()) {
             // for directories, recursively call
             File[] filesInDir = file.listFiles();
@@ -74,32 +79,21 @@ public class Train {
             for (int i = 0; i < filesInDir.length; i++) {
                 processFileHam(filesInDir[i]);
             }
-            System.out.println(file);
         } else {
             // for single files, load the words and count
             Scanner scanner = new Scanner(file);
             while (scanner.hasNext()) {
                 String word = scanner.next();
                 if (isWord(word)) {
-                    if(isUniqueHam(word,hamTree)){
+                    if(isUniqueHam(word)){
                     }
                 }
             }
         }
-        ProbabilityHam();
+
     }
 
-   /* private int countWord(String word) {
-        if (wordCounter.containsKey(word)) {
-            // increment the count
-            int oldCount = wordCounter.get(word);
-            wordCounter.put(word, oldCount + 1);
-        } else {
-            // add the word with count of 1
-            wordCounter.put(word, 1);
-        }
-        return
-    } */
+
 
     private boolean isWord(String token) {
         String pattern = "^[a-zA-Z]*$";
@@ -110,31 +104,31 @@ public class Train {
         }
     }
 
-    private boolean isUniqueHam(String token,TreeMap wordCounter)
+    private boolean isUniqueHam(String token)
     {
         String key = token;
-        if(wordCounter.get(key) == null) {
+        if(hamTree.get(key) == null) {
 
-            wordCounter.put(key, 1);
+            hamTree.put(key, 1);
             return true;
         }
         else {
             int i = hamTree.get(key);
-            wordCounter.put(key, (hamTree.get(key))+1);
+            hamTree.put(key, (hamTree.get(key))+1);
             return false;
         }
     }
-    private boolean isUniqueSpam(String token,TreeMap wordCounter)
+    private boolean isUniqueSpam(String token)
     {
         String key = token;
-        if(wordCounter.get(key) == null) {
+        if(spamTree.get(key) == null) {
 
-            wordCounter.put(key, 1);
+            spamTree.put(key, 1);
             return true;
         }
         else {
             int i = spamTree.get(key);
-            wordCounter.put(key, (spamTree.get(key))+1);
+            spamTree.put(key, (spamTree.get(key))+1);
             return false;
         }
     }
@@ -179,13 +173,13 @@ public class Train {
 
         }
     }
-    public TreeMap<String, Double>TotalProb() {
+    public void TotalProb(){
         Double totalProb = 0.0;
         Double ValueSpamP;
         Double ValueHamP;
-        TreeMap<String, Double> TotalProb = new TreeMap<>();
+        TreeMap<String, Double>TotalProb = new TreeMap<>();
         this.TotalProb = TotalProb;
-        for (Map.Entry<String, Double> entry : HamProb.entrySet()) {
+        for (Map.Entry<String, Double>entry:HamProb.entrySet()) {
             String key = entry.getKey();
             ValueHamP = entry.getValue();
 
@@ -200,14 +194,17 @@ public class Train {
             }
             TotalProb.put(key, totalProb);
         }
-        for (Map.Entry<String, Double> entry1 : SpamProb.entrySet()) {
+        for (Map.Entry<String, Double>entry1:SpamProb.entrySet())
+        {
             String key = entry1.getKey();
             ValueSpamP = entry1.getValue();
-            if (isUniqueTotal(key, TotalProb)) {
-                if (HamProb.get(key) == null) {
+            if (isUniqueTotal(key,TotalProb)){
+                if(HamProb.get(key) == null){
                     totalProb = 1.0;
-                } else {
-                    Map.Entry<String, Double> entry = (Map.Entry<String, Double>) HamProb.entrySet();
+                }
+                else
+                {
+                    Map.Entry<String, Double>entry = (Map.Entry<String, Double>) HamProb.entrySet();
                     ValueHamP = entry.getValue();
 
                     totalProb = ValueSpamP / (ValueHamP + ValueSpamP);
@@ -216,49 +213,7 @@ public class Train {
                 TotalProb.put(key, totalProb);
             }
         }
-        return TotalProb;
     }
 }
 
 
-    /*public void printWordCounts(int minCount, File outFile) throws IOException {
-        if (!outFile.exists() || outFile.canWrite()) {
-            PrintWriter fout = new PrintWriter(outFile);
-
-            Set<String> keys = wordCounter.keySet();
-            Iterator<String> keyIterator = keys.iterator();
-            while (keyIterator.hasNext()) {
-                String key = keyIterator.next();
-                int count = wordCounter.get(key);
-
-                if (count >= minCount) {
-                    fout.println("'" + key + "' -> '" + count + "'");
-                }
-            }
-
-            fout.close();
-        }
-    }
-
-    /*public static void main(String[] args) {
-        if (args.length < 2) {
-            System.err.println("Usage: java WordCounter <input_dir> <output_file>");
-            System.exit(0);
-        }
-
-        Train wordCounter = new Train();
-        File inputDir = new File(args[0]);
-
-        try {
-            if (inputDir.exists()) {
-                // read the file
-                wordCounter.processFile(inputDir);
-
-                // handle the output
-                wordCounter.printWordCounts(2, new File(args[1]));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-} */
